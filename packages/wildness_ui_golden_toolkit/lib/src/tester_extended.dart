6877b82599015d0927df5a7678e72d82d4fc88c6
Jesus Bernabeu <jesus.bernabeu@finetwork.com>
2026-02-16 08:32:56 +0100

    update: add pumpAndSettle

--- packages/wildness_ui_golden_toolkit/lib/src/tester_extended.dart
+++ packages/wildness_ui_golden_toolkit/lib/src/tester_extended.dart
@@ -39,7 +39,7 @@ extension GoldenTesterExt on WidgetTester {
     await gestureBuilder?.call();
 
     await pump();
-
+    await pumpAndSettle();
     expect(find.byWidget(widget), matchesGoldenFile(_screenName(groupTitle)));
   }
 
