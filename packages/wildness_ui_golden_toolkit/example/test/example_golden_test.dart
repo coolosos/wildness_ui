abb7f61e96c09cde6c0e61c109a5eeb0e838fc66
Jesus Bernabeu <jesus.bernabeu@finetwork.com>
2026-02-18 10:55:55 +0100

    update: change how is showed test devices

--- packages/wildness_ui_golden_toolkit/example/test/example_golden_test.dart
+++ packages/wildness_ui_golden_toolkit/example/test/example_golden_test.dart
@@ -41,5 +41,28 @@ void main() {
         ),
       ],
     );
+    testDevicesGolden(
+      groupName: "test_devices_group",
+      name: 'test_devices_golden',
+      devices: [
+        TestDevice(name: 'iPhone 12', size: const Size(390, 844)),
+        TestDevice(name: 'iPad Pro', size: const Size(1024, 1366)),
+      ],
+      scenarios: [
+        Component(
+          name: 'test',
+          widget: Column(
+            children: [
+              LinearProgressIndicator(
+                value: 0.5,
+                minHeight: 4.0,
+                color: Color.fromRGBO(0, 0, 0, 1),
+                backgroundColor: Color.fromRGBO(255, 0, 0, 1),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
   });
 }
