57d1ce7e8c493bd19aff6b4785dd906c8a4b07d2
Jesus Bernabeu <jesus.bernabeu@finetwork.com>
2026-02-19 10:02:38 +0100

    update: remove unused files and use linter

--- packages/wildness_ui_golden_toolkit/example/test/example_golden_test.dart
+++ packages/wildness_ui_golden_toolkit/example/test/example_golden_test.dart
@@ -4,13 +4,13 @@ import 'package:wildness_ui_golden_toolkit/wildness_ui_golden_toolkit.dart';
 void main() {
   group('group_name', () {
     testColumnComponent(
-      groupName: "test_component_column_group",
+      groupName: 'test_component_column_group',
       name: 'test_component_column',
       surfaceSize: const Size(800, 300),
       scenarios: [
         Component(
           name: 'test',
-          widget: LinearProgressIndicator(
+          widget: const LinearProgressIndicator(
             value: 0.5,
             color: Color.fromRGBO(0, 0, 0, 1),
             backgroundColor: Color.fromRGBO(255, 0, 0, 1),
@@ -19,20 +19,20 @@ void main() {
       ],
     );
     testDeviceComponent(
-      groupName: "test_device_component_group",
+      groupName: 'test_device_component_group',
       name: 'test_device_component',
       devices: [
-        TestDevice(name: 'iPhone 12', size: const Size(390, 844)),
-        TestDevice(name: 'iPad Pro', size: const Size(1024, 1366)),
+        const TestDevice(name: 'iPhone 12', size: Size(390, 844)),
+        const TestDevice(name: 'iPad Pro', size: Size(1024, 1366)),
       ],
       scenarios: [
         Component(
           name: 'test',
-          widget: Column(
+          widget: const Column(
             children: [
               LinearProgressIndicator(
                 value: 0.5,
-                minHeight: 4.0,
+                minHeight: 4,
                 color: Color.fromRGBO(0, 0, 0, 1),
                 backgroundColor: Color.fromRGBO(255, 0, 0, 1),
               ),
@@ -42,20 +42,20 @@ void main() {
       ],
     );
     testDevicesGolden(
-      groupName: "test_devices_group",
+      groupName: 'test_devices_group',
       name: 'test_devices_golden',
       devices: [
-        TestDevice(name: 'iPhone 12', size: const Size(390, 844)),
-        TestDevice(name: 'iPad Pro', size: const Size(1024, 1366)),
+        const TestDevice(name: 'iPhone 12', size: Size(390, 844)),
+        const TestDevice(name: 'iPad Pro', size: Size(1024, 1366)),
       ],
       scenarios: [
         Component(
           name: 'test',
-          widget: Column(
+          widget: const Column(
             children: [
               LinearProgressIndicator(
                 value: 0.5,
-                minHeight: 4.0,
+                minHeight: 4,
                 color: Color.fromRGBO(0, 0, 0, 1),
                 backgroundColor: Color.fromRGBO(255, 0, 0, 1),
               ),
